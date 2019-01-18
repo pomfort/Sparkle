ead07c93e353a00c241a3ce33056093d6d7c311b
Simon Wagner <wagner.sim1988@googlemail.com>
2019-01-18 13:19:40 +0100

    Example for how to use and handle custom update validation errors

--- TestApplication/SUUpdateSettingsWindowController.m
+++ TestApplication/SUUpdateSettingsWindowController.m
@@ -9,7 +9,7 @@
 #import "SUUpdateSettingsWindowController.h"
 #import <Sparkle/Sparkle.h>
 
-@interface SUUpdateSettingsWindowController ()
+@interface SUUpdateSettingsWindowController ()<SUUpdaterDelegate>
 
 @property (nonatomic) IBOutlet SUUpdater *updater;
 
@@ -18,6 +18,11 @@
 @implementation SUUpdateSettingsWindowController
 
 @synthesize updater = _updater;
+
+- (void)awakeFromNib
+{
+    self.updater.delegate = self;
+}
 
 - (NSString *)windowNibName
 {
@@ -35,6 +40,57 @@
         return [self.updater validateMenuItem:menuItem];
     }
     return YES;
+}
+
+#pragma mark - Sparkle Delegate
+
+- (BOOL)itemContainsValidUpdate:(SUAppcastItem *)item forUpdater:(__unused SUUpdater *)updater validationError:(NSError * _Nullable __autoreleasing *)validationError
+{
+    NSDictionary *envelope = [item.propertiesDictionary objectForKey:@"enclosure"];
+    NSString *majorVersionString = [envelope objectForKey:@"major-version"];
+    int majorVersion = [majorVersionString intValue];
+
+    if(majorVersion > 1) {
+        if(validationError != nil) {
+            *validationError = [NSError errorWithDomain:@"com.custom.validation" code:-1 userInfo:@{
+                                                                                                    @"major-version" : majorVersionString,
+                                                                                                    }];
+        }
+        return NO;
+    }
+    else {
+        return YES;
+    }
+}
+
+- (BOOL)showUpdateValidationError:(NSError *)validationError forUpdater:(__unused SUUpdater *)updater
+{
+    if([validationError.domain isEqualToString:@"com.custom.validation"]) {
+        if(validationError.code == -1) {
+            NSString *majorVersion = [validationError.userInfo valueForKey:@"major-version"];
+
+            NSAlert *alert = [[NSAlert alloc] init];
+            alert.messageText = [NSString stringWithFormat:@"New Major Version %@ has been released", majorVersion];
+            alert.informativeText = @"Download the newest version from our website";
+            [alert addButtonWithTitle:@"Download new version"];
+            [alert addButtonWithTitle:@"Close"];
+
+            NSModalResponse response = [alert runModal];
+
+            if(response == NSAlertFirstButtonReturn) {
+                NSURL *downloadURL = [NSURL URLWithString:@"https://pomfort.com/livegradepro/what-is-new/"];
+                [[NSWorkspace sharedWorkspace] openURL:downloadURL];
+            }
+
+            return YES;
+        }
+        else {
+            return NO;
+        }
+    }
+    else {
+        return NO;
+    }
 }
 
 @end
